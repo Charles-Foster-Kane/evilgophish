40b6e4b24b9e7165804ac827e7c85dda26455b51
Ramòn Janssen <Charles-Foster-Kane@users.noreply.github.com>
2024-04-09 09:32:46 +0200

    Update maillog.go
    Stop leaking internal hostname

--- gophish/models/maillog.go
+++ gophish/models/maillog.go
@@ -308,13 +308,13 @@ var maxBigInt = big.NewInt(math.MaxInt64)
 
 // generateMessageID generates and returns a string suitable for an RFC 2822
 // compliant Message-ID, e.g.:
-// <1444789264909237300.3464.1819418242800517193@DESKTOP01>
+// <1444789264909237300.3464.1819418242800517193@mail-out.ovh.net>
 //
 // The following parameters are used to generate a Message-ID:
 // - The nanoseconds since Epoch
 // - The calling PID
 // - A cryptographically random int64
-// - The sending hostname
+// - A mail server hostname
 func (m *MailLog) generateMessageID() (string, error) {
 	t := time.Now().UnixNano()
 	pid := os.Getpid()
@@ -322,10 +322,10 @@ func (m *MailLog) generateMessageID() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	h, err := os.Hostname()
+	h, err := "mail-out.ovh.net"
 	// If we can't get the hostname, we'll use localhost
 	if err != nil {
-		h = "localhost.localdomain"
+		h = "mail-out.ovh.net"
 	}
 	msgid := fmt.Sprintf("<%d.%d.%d@%s>", t, pid, rint, h)
 	return msgid, nil
